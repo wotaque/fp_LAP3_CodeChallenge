c2b1ccfd2d153170108545400a3a93e21666de24
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2022-02-02 14:40:43 +0000

    specific repo details

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Users from './components/Users';
 
+
 function App() {
   return (
     <div className="App">
 
       <header className="App-header">
         <h1>GitHub Search</h1>
-        
+    
       </header>
       <main className='App-main'>
         <Users />
--- my-app/src/components/Users.js
+++ my-app/src/components/Users.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-
-
-
+import RepoDetails from './RepoDetails';
 
 const Users = () => {
     const [username, setUsername] = useState('');
     const [loading, setLoading] = useState(false);
     const [repos, setRepos] = useState([]);
+
+    const [details, setDetails] = useState({});
+    const [detailsLoading, setDetailsLoading] = useState(false);
+
+    useEffect(() => {
+        setRepos([]);
+        setDetails({});
+
+    }, [username]);
+
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -29,7 +37,11 @@ const Users = () => {
 
     function renderRepo(repo) {
         return(
-            <div className='row' key={repo.id}>
+            <div 
+                className='row' 
+                onClick={() => getDetails(repo.name)}
+                key={repo.id}
+            >
                 <h2 className='repo-name'>
                     Hello
                 {repo.name}
@@ -38,29 +50,44 @@ const Users = () => {
 
         )
     }
+    function getDetails(repoName) {
+        setDetailsLoading(true);
+        axios({
+            method: 'get',
+            url: `https://api.github.com/repos/${username}/${repoName}`,
+        }).then(res => {
+            setDetailsLoading(false);
+            setDetails(res.data);
+        });        
+        
+    }
     
+    return (
+        <div className='page'>
+            <div className='landing-page-container'>
+                <div className='left-side'>
+                    <form className='form'>
+                        <input 
+                            className='input'
+                            value={username} 
+                            onChange={e => {setUsername(e.target.value)}}
+                            placeholder='GitHub Name'
+                        />
+                        <button className='button' onClick={handleSubmit}>{loading ? 'Loading...' : 'Search'}</button>
+                    </form>
 
+                    <div className='results'>
+                        <p>results</p>
+                        {repos.map(renderRepo)}
+                    </div>
+                </div>
+            
+                <RepoDetails details={details} loading={detailsLoading} />
 
-  return (
-  <div>
-      <form className='form'>
-        <input 
-        className='input'
-        value={username} 
-        onChange={e => {setUsername(e.target.value)}}
-        placeholder='GitHub Name'
-        />
-        <button className='button' onClick={handleSubmit}>{loading ? 'Loading...' : 'Search'}</button>
+            </div>
 
-      </form>
-      <div className='results'>
-        <p>results</p>
-        {repos.map(renderRepo)}
-        
-        
-      </div>
-
-  </div>
-  )};
+        </div>
+    )
+};
 
 export default Users;
